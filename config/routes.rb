b471d1bdf4605ef2b8036336d5be633d47a92b03
JoseCarlos-17 <josecarlosbsilva.172@gmail.com>
2025-04-16 22:48:46 -0300

    adding create admin router in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     registrations: 'auth/registrations',
     sessions: 'auth/sessions'
   }
+
+  namespace :admin do
+    resources :artists, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
