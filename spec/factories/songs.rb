081db8a72389838f8a44df8a6e867df4820017bb
JoseCarlos-17 <josecarlosbsilva.172@gmail.com>
2025-06-11 10:12:08 -0300

    update song action have been created

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :song do
     title { "MyString" }
     duration { "00:02:29" }
-    date { "2025-02-06" }
+    release_date { "2025-02-06" }
     file_url { "MyString" }
   end
 end
