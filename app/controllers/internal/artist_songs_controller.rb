439cee587b8654e56aa876376e4e3793bd90c21e
JoseCarlos-17 <josecarlosbsilva.172@gmail.com>
2026-01-21 23:25:57 -0300

    the create artist song endpoint have been created

--- app/controllers/internal/artist_songs_controller.rb
+++ app/controllers/internal/artist_songs_controller.rb
@@ -1,2 +1,16 @@
 class Internal::ArtistSongsController < ApplicationController
+  
+  def create
+    artist_song = ArtistSong.create!(artist_song_params)
+
+    render json: artist_song,
+           serializer: Internal::ArtistSongs::Create::ArtistSongSerializer,
+           status: :created
+  end
+
+  private
+
+  def artist_song_params
+    params.require(:artist_song).permit(:artist_id, :song_id)
+  end
 end
--- spec/requests/internal/artist_songs_spec.rb
+++ spec/requests/internal/artist_songs_spec.rb
@@ -1,7 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe "Internal::ArtistSongs", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Internal::ArtistSongs', type: :request do
+  describe 'POST#create' do
+    context 'when an artist song is created' do
+      let!(:artist) { create(:artist) }
+      let!(:song) { create(:song) }
+
+      let(:artist_song_params) { attributes_for(:artist_song,
+        artist_id: artist.id, song_id: song.id) }
+
+      before do
+        post '/internal/artist_songs',
+        params: { artist_song: artist_song_params }
+      end
+
+      it 'must return 201 status code' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'must return artist_song attributes' do
+        expect(json_body).to include(:id, :artist, :song)
+      end
+
+      it 'must return artist attributes' do
+        expect(json_body[:artist]).to include(:id, :name, :country)
+      end
+
+      it 'must return song attributes' do
+        expect(json_body[:song]).to include(:id, :title, :duration,
+                                            :release_date, :file_url)
+      end
+    end
+
+    context 'when an artist song is not created' do
+      let!(:artist) { create(:artist) }
+      let!(:song) { create(:song) }
+
+      let(:artist_song_invalid_params) { attributes_for(:artist_song,
+        artist_id: artist.id, song_id: nil) }
+
+      before do
+        post '/internal/artist_songs',
+        params: { artist_song: artist_song_invalid_params }
+      end
+
+      it 'must return 422 status code' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'must return error message' do
+        expect(json_body).to have_key(:errors)
+      end
+    end
   end
 end
